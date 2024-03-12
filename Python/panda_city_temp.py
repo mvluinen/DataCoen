6190c8cb93d24f2579e66f8e5e033223d34663f2
senna <sennabrons@hotmail.com>
2024-03-12 12:25:48 +0100

    Hello

--- Python/panda_city_temp.py
+++ Python/panda_city_temp.py
@@ -1,4 +1,12 @@
 import pandas
 
 data = pandas.read_csv('Datasets/city_temperature.csv')
+data.drop(['State', 'Region'], axis=1, inplace=True)
+data = data.drop(data[data['AvgTemperature'] == -99].index)
+new_data = data.groupby(['City', 'Country'])['AvgTemperature'].mean()
+
+print(new_data.tail(10))
 print(data.tail(10))
+# print(data.dtypes)
+# print(new_data.info)
+# print(data.describe())
